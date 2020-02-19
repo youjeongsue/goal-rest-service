ce7e54dc7b592bda05f0d2519623b4279bc97286
jiwon <wldnjs602@ajou.ac.kr>
2020-02-20 01:32:44 +0900

    Remove conflict code

--- src/main/java/com/goal/restservice/repository/GoalRepository.java
+++ src/main/java/com/goal/restservice/repository/GoalRepository.java
@@ -5,16 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
 public interface GoalRepository extends JpaRepository<Goal, Long> {
 
   List<Goal> findByUserIdOrderByModifiedDateDesc(long id);
 //  Optional<List<Goal>> findByUserIdOrderByModifiedDateDesc(long id);
 }
-||||||| e3eb698
-public interface GoalRepository extends JpaRepository<Goal, Long> {
-
-}
-=======
-public interface GoalRepository extends JpaRepository<Goal, Long> {}
->>>>>>> origin/develop
